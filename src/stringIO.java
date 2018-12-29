67ac3748caf89b76ebad878f13b099616ee9f483
DPS0340 <jh001007@gmail.com>
2018-12-29 23:58:32 +0900

    init

---
+++ src/stringIO.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class stringIO {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        while(line != null && !line.isEmpty()) {
+            System.out.println(line);
+            line = sc.nextLine();
+        }
+        System.out.println("End!");
+    }
+}
